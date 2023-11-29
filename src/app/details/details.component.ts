30f86a294e53e65af22512f34a91594a237f45da
Oskar Jaworski <ojaworski@rockitworks.pl>
2023-11-30 00:06:09 +0100

    lab-a done

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,24 @@
+import {Component, inject} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {HousingService} from "../housing.service";
+import {HousingLocation} from "../housinglocation";
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
+  }
+}
